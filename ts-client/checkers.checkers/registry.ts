40980ec1be2e7ac58d07ed2ad5bfb24ea53f9174
Michaël <90640967+michaellandais@users.noreply.github.com>
2022-11-11 12:05:32 +0100

    Scaffolded createPost massage

--- ts-client/checkers.checkers/registry.ts
+++ ts-client/checkers.checkers/registry.ts
@@ -1,6 +1,8 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreatePost } from "./types/checkers/checkers/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/checkers.checkers.MsgCreatePost", MsgCreatePost],
     
 ];
 
