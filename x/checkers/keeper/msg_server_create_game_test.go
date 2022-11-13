244c79f0b7ef7d757e7a11e91f78333306bf0b17
Michaël <90640967+michaellandais@users.noreply.github.com>
2022-11-13 11:38:59 +0100

    Update msg_server_create_game_test.go

--- x/checkers/keeper/msg_server_create_game_test.go
+++ x/checkers/keeper/msg_server_create_game_test.go
@@ -1,10 +1,15 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	keepertest "checkers/testutil/keeper"
+	"checkers/x/checkers"
+	"checkers/x/checkers/keeper"
 	"checkers/x/checkers/types"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,7 +20,7 @@ const (
 )
 
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -23,6 +28,35 @@ func TestCreateGame(t *testing.T) {
 	})
 	require.Nil(t, err)
 	require.EqualValues(t, types.MsgCreateGameResponse{
-		GameIndex: "", // TODO: update with a proper value when updated
+		GameIndex: "1", // TODO: update with a proper value when updated
 	}, *createResponse)
+}
+
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
+func TestCreate1GameHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 2,
+	}, systemInfo)
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: bob,
+		Red:   carol,
+	}, game1)
 }
